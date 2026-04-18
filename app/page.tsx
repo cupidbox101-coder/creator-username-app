ef9419048ac9ef2fd82c65227144a0539c9f8638
glory bhanu <glorybhanu@MacBookPro.lan>
2026-04-18 22:23:53 +0530

    fix final error

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-export default function Home() {
-  return (
-    <main style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>CupidBox 💝</h1>
-      <p>Send anonymous gifts to your favorite creators</p>
-      <button onClick={() => alert("Coming soon 🚀")}>
-  Get your Cupid Address
-</button>
-    </main>
-  );
-}
+return (
+  <main style={{ textAlign: "center", marginTop: "100px" }}>
+    <h1>CupidBox 💝</h1>
+    <p>Send anonymous gifts to your favorite creators</p>
+
+    <button onClick={() => alert("Coming soon 🚀")}>
+      Get your Cupid Address
+    </button>
+  </main>
+);
