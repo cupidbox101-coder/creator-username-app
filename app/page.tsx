dbcdad697b3f4a18b4562cb83f560be96bc7637b
glory bhanu <glorybhanu@MacBookPro.lan>
2026-04-18 22:18:02 +0530

    button working

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ export default function Home() {
     <main style={{ textAlign: "center", marginTop: "100px" }}>
       <h1>CupidBox 💝</h1>
       <p>Send anonymous gifts to your favorite creators</p>
-      <button>Get your Cupid Address</button>
+      <button onClick={() => alert("Coming soon 🚀")}>
+  Get your Cupid Address
+</button>
     </main>
   );
 }
